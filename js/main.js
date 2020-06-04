b4bb9d398b5513ac6a9e5193990c9dccd3faad77
Soraya Valle <svalleortega@gmail.com>
2020-06-04 12:45:42 +0200

    Add function to toggle panels

--- js/main.js
+++ js/main.js
@@ -1,2 +1,9 @@
 'use strict';
 
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
